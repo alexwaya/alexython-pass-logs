4ddbff05583c8742bfe69059f14205b09b385168
alexwaya <alex.waya@gmail.com>
2018-06-08 15:56:46 +0300

    add landing page

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #Import the module flask
-from flask import Flask
-app = Flask(_name_)
+
+from flask import Flask, render_template
+app = Flask(__name__)
 
 #define the basic route / and its corresponding request handler
 @app.route("/")
 def main():
-	return "Welcome!"
+	return render_template("index.html")
 
 #check if the executed file is the main program and run the app
 if __name__ == '__main__':
